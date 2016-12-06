e1dcf5ec69706d2ff82478aed49fbbcf27f74025
MatKulis <mateusz.gmali@gmail.com>
2016-12-06 18:34:05 +0100

    Add files via upload

---
+++ KiK/KiK/MainForm.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 2016-11-30
+ * Time: 18:27
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KiK
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+	
+		}
+		void ZasadyGryToolStripMenuItemClick(object sender, EventArgs e)
+		{
+		
+		}
+		void AutorzyToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		void WyjścieToolStripMenuItemClick(object sender, EventArgs e)
+		{
+		
+		}
+	}
+}
